2d2d0c1d67769020b48016ac17166c8f2d3a0591
viveksingh10 <viveksingh9780@gmail.com>
2020-10-01 23:39:30 +0530

    Updated DB and User Page

--- src/application/UserController.java
+++ src/application/UserController.java
@@ -9,6 +9,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
@@ -17,11 +18,54 @@ public class UserController implements Initializable {
 
 	@FXML
 	private Label userLBL;
-	@Override
-	public void initialize(URL arg0, ResourceBundle arg1) {
-		// TODO Auto-generated method stub
+	@FXML
+	private CheckBox cb1;
+	@FXML
+	private CheckBox cb2;
+	@FXML
+	private CheckBox cb3;
+	@FXML
+	private CheckBox cb4;
+	@FXML
+	private CheckBox cb5;
+	@FXML
+	private Label lbllist;
+	@FXML
+	private Label lblcount;
+	@FXML
+	private Label myy;
+	
+	
+	
+	public void checkevent(ActionEvent event) {
+		String message = "";
+		int count = 0;
+		if (cb1.isSelected()){
+			count ++;
+			message += cb1.getText() + "\n";
+		}
+		if (cb2.isSelected()){ 
+			count++;
+			message += cb2.getText() + "\n";
+		} 
+		if (cb3.isSelected()){
+			count ++;
+			message += cb3.getText() + "\n";
+		}
+		if (cb4.isSelected()){
+			count ++;
+			message += cb4.getText() + "\n";
+		}
+		if (cb5.isSelected()){
+			count ++;
+			message += cb5.getText() + "\n";
+		}
 		
+		lblcount.setText("Completed : " + count);
+		lbllist.setText(message);
 	}
+	
+	
 	
 	
 	public void GetUser(String user) {
@@ -43,5 +87,26 @@ public class UserController implements Initializable {
 		} catch (Exception e) {
 			
 		}
+	}
+	
+public void exxit(ActionEvent event) {
+		System.exit(0);
+}
+
+public void submit(ActionEvent event) {
+
+	if(lbllist.getText().trim().isEmpty()){
+		myy.setText("Nothing completed yet");
+	}
+	else{
+	myy.setText("Updates Saved");
+	}
+	
+}
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		
 	}
 }
